27dc641ca26a5e44e5f66ef34e7d8ae1182f624b
Simon Pesant <pesantsi@hotmail.comn>
2020-02-28 17:22:51 -0500

    rename

--- Gui/TerminalGuiMenu.cs
+++ Gui/TerminalGuiMenu.cs
@@ -8,8 +8,32 @@ namespace CustomKeyboardProject.Gui
     public class TerminalGuiMenu : MenuBar
     {
         public TerminalGuiMenu()
-           : base(null)
+           : base(new MenuBarItem[1])
         {
+            Menus[0] = new MenuBarItem("_File", new MenuItem[] {
+                    new MenuItem ("_Open Bitmp File", "", OpenBitmapFile),
+                    new MenuItem ("_Quit", "", () => 
+                    { 
+                        if (Quit ()) 
+                            Application.Top.Running = false; 
+                    })
+                });
+        
+        }
+
+        private void OpenBitmapFile()
+        {
+            var d = new OpenDialog("Open Bitmap File", "Open bitmap file for the OLed display");
+            d.AllowedFileTypes = new string[] { "*.bmp" };
+            d.AllowsMultipleSelection = false;
+
+            Application.Run(d);
+        }
+
+        private bool Quit()
+        {
+            var n = MessageBox.Query(75, 7, "Quit Keyboard Controller", "Are you sure you want to quit the Keyboard Controller?", "Yes", "No");
+            return n == 0;
         }
     }
 }
